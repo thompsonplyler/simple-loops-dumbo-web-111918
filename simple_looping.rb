38985dd2e4bf327eda19ee788df07e97f4de0611
Thompson Plyler <nealok.g@gmail.com>
2018-10-18 21:20:22 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,6 +11,7 @@ end
 end
 
 def times_iterator(number_of_times)
+  number_of_times.do 
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
